8ec868dd56baaa669f7b035a47d54bcd4bc14ce0
dedisvoin <88434293+dedisvoin@users.noreply.github.com>
2025-05-30 15:21:18 +0300

    from angela

--- src/ai/core.py
+++ src/ai/core.py
@@ -1,8 +1,10 @@
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 
+
+from typing import Any
 
 
 def loadPDF(file_path: str):
@@ -16,3 +18,10 @@ def split_for_chanks(chunk_size: int, chunk_overlap: int, pages):
     chunks = text_splitter.split_documents(pages)
     return chunks
 
+
+PDF = loadPDF("data\eatwell_guide_annex_1.pdf")
+PDF_CHUNKS = split_for_chanks(chunk_size=500, chunk_overlap=100, pages=PDF)
+
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = Chroma.from_documents(PDF_CHUNKS, embeddings, persist_directory="db")
