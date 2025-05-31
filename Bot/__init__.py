c2beb9dbdca310d7e1c96b0b523eda122c3f488a
Roman Semenov <semenich212166@gmail.com>
2025-05-31 13:38:35 +0300

    Add  sending prompts to ai

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,13 +1,16 @@
 from aiogram import Bot, Dispatcher
 from Config.bot import BOT_TOKEN
 from .routers.user import user_router
+from .routers.base import base_router, prompts_router
 from .middlewares.user import ProfileMiddleware
 from aiogram.fsm.storage.memory import MemoryStorage
 
 bot = Bot(token=BOT_TOKEN)
 
 dp = Dispatcher(storage=MemoryStorage())
+dp.include_router(base_router)
 dp.include_router(user_router)
+dp.include_router(prompts_router)
 dp.message.middleware(ProfileMiddleware())
 dp.callback_query.middleware(ProfileMiddleware())
 
